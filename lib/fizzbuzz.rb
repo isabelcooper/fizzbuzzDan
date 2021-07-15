89718d8482a1706c4b093a5c20db67d2446f28c1
isabelcooper <isabelcooper093@gmail.com>
2021-07-15 18:25:00 +0100

    setting the fizzbuzz response given a number

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,7 @@
-def fizzbuzz
-  puts 'code me'
+def fizzbuzz(number)
+  output = ''
+  output += 'fizz' if number % 3 == 0
+  output += 'buzz' if number % 5 == 0
+
+  output == '' ? number : output
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,7 +2,25 @@ require 'rspec'
 require './lib/fizzbuzz'
 
 RSpec.describe 'fizzbuzz' do
-  it 'does nothing right now' do
-    expect(fizzbuzz).to eq(nil)
+  it 'returns the number' do
+    expect(fizzbuzz(1)).to eq(1)
+    expect(fizzbuzz(2)).to eq(2)
+  end
+
+  it 'returns fizz' do
+    expect(fizzbuzz(3)).to eq('fizz')
+    expect(fizzbuzz(12)).to eq('fizz')
+  end
+
+  it 'returns buzz' do
+    expect(fizzbuzz(5)).to eq('buzz')
+    expect(fizzbuzz(100)).to eq('buzz')
+  end
+
+  it 'returns fizzbuzz' do
+    expect(fizzbuzz(15)).to eq('fizzbuzz')
+    expect(fizzbuzz(30)).to eq('fizzbuzz')
+    expect(fizzbuzz(45)).to eq('fizzbuzz')
+    expect(fizzbuzz(60)).to eq('fizzbuzz')
   end
 end
